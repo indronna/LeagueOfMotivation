868679830e57691691d396c4600a499a5ea794e0
Vincent <fbampli@gmail.com>
2020-04-20 19:16:35 +0200

    Death detection implemented
    Now, when you are dead, the program write "You are DEAD"

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.awt.AWTException;
+import java.awt.Color;
 import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Toolkit;
@@ -9,18 +10,25 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class Main {
-    
-    static int xMax = 0;
-    static int yMax = 0;
+	// HardCoded coordinate for the red counter of the death
+    static int xMin = 662;
+    static int yMin = 1012;
+    static int xMax = 688;
+    static int yMax = 1030;
+    static Boolean loop = true;
 
-    public static void main(String[] args) throws IOException, AWTException {
-        while(true) {
+    public static void main(String[] args) throws IOException, AWTException, InterruptedException {
+        while(loop) {
             Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
             BufferedImage capture = new Robot().createScreenCapture(screenRect);
             ImageIO.write(capture, "bmp", new File(".\\image.bmp"));
-            for (int x = 0; x < xMax; x++) {
-                
+            for (int x = xMin; x < xMax; x++) {
+                for (int y = xMax; y < yMax; y++) {
+                	if (new Color(capture.getRGB(x, y)).getRed() == 255)
+                	System.out.println("You are DEAD");
+                }
             }
+        Thread.sleep(1000);
         }
         
         //capture.getRGB(x, y)
